60772a9d756c97327a1c5206bf578e1c985160d1
Nakulc18 <nakulc2006@gmail.com>
2025-11-29 15:37:06 +0530

    Day 10 code pushed

---
+++ Day 10/4_doubcirLL.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+struct Node *head, *newNode, *tail;
+int main()
+{
+    head = NULL;
+    int ch = 1;
+    while (ch)
+    {
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        printf("Enter a number: ");
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+
+            tail = newNode;
+        }
+        else
+        {
+
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        printf("Do you want to continue(0|1): ");
+        scanf("%d", &ch);
+    }
+    printf("Elments: \n");
+    tail = head;
+
+    while (tail->next != head)
+    {
+        printf("%d\n", tail->data);
+        tail = tail->next;
+    }
+    printf("%d\n", tail->data);
+    printf("\n");
+    return 0;
+}
